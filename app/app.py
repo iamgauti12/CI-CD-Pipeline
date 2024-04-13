114c0bc1779cd21dcf1dca8df05a0d5a4fd59d7a
Gautam Tiwari <95685638+iamgauti12@users.noreply.github.com>
2024-04-13 12:22:21 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(_name_)
+
+# Sample to-do list
+todos = []
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! This is my Flask App.'
+def index():
+    return render_template('index.html', todos=todos)
 
-if __name__ == '__main__':
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form['todo']
+    todos.append(todo)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete_todo(index):
+    del todos[index]
+    return redirect(url_for('index'))
+
+if _name_ == '_main_':
     app.run(debug=True, host='0.0.0.0')
