bbe4ac6d4563ce82d45479c693ada4a9584741ff
Gautam Tiwari <95685638+iamgauti12@users.noreply.github.com>
2024-04-13 13:05:45 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,10 +13,22 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate():
+    names = request.form.getlist('name')
+    if names:
+        nickname = generate_nickname(names)
+    else:
+        nickname = generate_random_nickname()
+    return render_template('index.html', nickname=nickname)
+
+def generate_random_nickname():
     adjective = random.choice(adjectives)
     noun = random.choice(nouns)
-    nickname = f'{adjective} {noun}'
-    return render_template('index.html', nickname=nickname)
+    return f'{adjective} {noun}'
+
+def generate_nickname(names):
+    adjective = random.choice(adjectives)
+    noun = random.choice(names)
+    return f'{adjective} {noun}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
